194d71a5a18a48bcbf3781802c36d00808a9ecda
Angelpintoo <ang.pintoh@duocuc.cl>
2024-06-03 17:31:44 -0400

    componente de factura agregado a pagina

---
+++ src/Components/factura/facturaform.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './factura.css'
+
+const facturaform = () => {
+    return (
+     <div>
+       <form class="form">
+           <div class="flex">
+              <p> Orden de compra</p>
+              <h5>Datos del proveedor </h5>
+              <label> 
+                 <input class="input" type="text" placeholder="" required=""/> 
+                 <span>nombre o razon social</span>
+              </label>
+
+              <label>
+                 <input class="input" type="text" placeholder="" required=""/>
+                 <span>CUIT</span>
+              </label>
+           </div>  
+            
+           <label>
+              <input class="input" type="email" placeholder="" required=""/>
+              <span>dirección </span>
+           </label> 
+        
+           <label>
+              <input class="input" type="text" placeholder="" required=""/>
+              <span>teléfono </span>
+           </label>
+           <label>
+              <textarea class="input01" placeholder="" rows="3" required=""></textarea>
+              <span>correo electronico</span>
+           </label>
+    
+           <button href="#" class="fancy">
+           <span class="top-key"></span>
+           <span class="text">ingresar</span>
+           <span class="bottom-key-1"></span>
+           <span class="bottom-key-2"></span>
+           </button>
+       </form>
+     </div>
+ )
+}
+
+export default facturaform
+
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import LoginComponent from '../Components/Login/Login'
+
+export default function Login() {
+  return (
+    <LoginComponent/>
+  )
+}
